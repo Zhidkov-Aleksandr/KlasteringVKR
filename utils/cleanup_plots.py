80c7aeffec545312abeef04efd178b719d044eff
Zhidkov-Aleksandr <dotenvargen@gmail.com>
2026-04-13 21:24:13 +0300

    commit

--- utils/cleanup_plots.py
+++ utils/cleanup_plots.py
@@ -2,18 +2,5 @@ import os
 import glob
 
 def cleanup_images():
-    """Удаляет старые изображения перед новым запуском."""
-    patterns = [
-        "output/districts/plots/*.png",
-        "output/districts/diagrams/*.png",
-        "output/regions/plots/*.png",
-        "output/regions/diagrams/*.png",
-        "output/all_regions/plots/*.png",
-        "output/all_regions/diagrams/*.png"
-    ]
-    for pattern in patterns:
-        for file in glob.glob(pattern):
-            try:
-                os.remove(file)
-            except OSError:
-                pass
+    """Больше не удаляем старые изображения здесь, так как мы управляем этим на уровне приложения Streamlit"""
+    pass
