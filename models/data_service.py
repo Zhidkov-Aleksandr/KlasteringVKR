2dafc41c5d3317a313b00fa6f0d3958bc1edd154
Zhidkov-Aleksandr <dotenvargen@gmail.com>
2026-04-18 15:34:28 +0300

    commit

--- models/data_service.py
+++ models/data_service.py
@@ -2,8 +2,6 @@ import pandas as pd
 from models.architecture import DataLoader
 from models.database import create_database
 from utils.excel_loader import load_excel
-from utils.district_clustering import get_district_matrix
-from utils.cleanup_plots import cleanup_images
 
 
 class DistrictDataLoader(DataLoader):
@@ -14,13 +12,9 @@ class DistrictDataLoader(DataLoader):
         # Создаем базу данных
         create_database()
 
-        # Удаляем старые изображения
-        cleanup_images()
 
         # Загружаем Excel
         load_excel(self.excel_file, year)
 
-        # Получаем матрицу округов
-        data = get_district_matrix(year)
 
-        return data
+        return pd.DataFrame()
--- utils/cleanup_plots.py
+++
@@ -1,6 +0,0 @@
-import os
-import glob
-
-def cleanup_images():
-    """Больше не удаляем старые изображения здесь, так как мы управляем этим на уровне приложения Streamlit"""
-    pass
